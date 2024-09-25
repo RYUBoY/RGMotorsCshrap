8bb540070a7af2a63a78dcf49f2f28932794a4fd
KimJongHoss <haberde97@gmail.com>
2024-09-25 13:24:22 +0900

    take template

--- RGMotors/MainWindow.xaml.cs
+++ RGMotors/MainWindow.xaml.cs
@@ -20,5 +20,58 @@ namespace RGMotors
         {
             InitializeComponent();
         }
+
+        private void buttonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(); // WPF 애플리케이션 종료
+        }
+
+        //관리자 모드
+
+        private void managerLoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            login();
+        }
+
+        private string adminID = "admin123";//관리자 아이디
+        private string adminPW = "admin123";//관리자 비밀번호
+        public void login()
+        {
+            if (idTextBox.Text == adminID)
+            {
+                if (pwTextBox.Password == adminPW)
+                {
+                    MessageBox.Show("로그인 완료!");
+                    loginGrid.Visibility = Visibility.Collapsed;
+                    managerPage.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageBox.Show("잘못된 비밀번호입니다.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("잘못된 아이디입니다.");
+            }
+        }
+
+        private void OpenLink_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://github.com/KimJongHoss/RGMotorsCshrap";  // 여기에 원하는 URL을 입력하세요
+            try
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true  // 이 옵션은 .NET Core와 .NET Framework에서 URL을 열 때 필요합니다
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link: " + ex.Message);
+            }
+        }
+
     }
 }
